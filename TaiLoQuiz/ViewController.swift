887694d6f02aeee584646abb5fc34db4f41c3a6b
A-Tan5 <paocd2971@gmail.com>
2020-08-01 20:25:43 +0800

    Initial Commit

---
+++ TaiLoQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaiLoQuiz
+//
+//  Created by tantsunsin on 2020/8/1.
+//  Copyright © 2020 tantsunsin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
